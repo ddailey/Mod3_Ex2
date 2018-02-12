b3cc06aafe8a6d3cfe9d19ca294c3e397823c7f1
ddailey <deandail@gmail.com>
2018-02-11 21:45:32 -0800

    Final

--- Mod3_Ex2.R
+++ Mod3_Ex2.R
@@ -18,10 +18,17 @@ mean(titanic_embarked$age, na.rm=TRUE)
 
 #Replace unknown ages with the mean
 titanic_ages <- titanic_embarked %>% mutate(age = ifelse(is.na(titanic_original$age), "30", age))
-View(titanic_ages)
 
 #Replace empty fields in the boat column with "None"
 titanic_boats <- titanic_ages %>% mutate(boat = ifelse(is.na(titanic_original$boat), "None", boat))
 View(titanic_boats)
 
+#Create new binary column to indicate cabin assignment
+#First create new column with cabin data
+titanic_hascabin <- mutate(titanic_boats, has_cabin = cabin)
 
+#Then change cabin data to 0 and 1
+titanic_hascabin <- titanic_hascabin %>% mutate(has_cabin = ifelse(is.na(titanic_hascabin$has_cabin), "0", 1))
+
+#Write new csv
+write_csv(titanic_hascabin, "titanic_clean.csv")
